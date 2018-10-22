df7f91cc3cdec9bf24a841e5b06036b9a93583aa
Alessandro Pisu <a.pisu@dstech.it>
2018-10-22 12:24:23 +0200

    fix

--- static/js/controller/indexController.js
+++ static/js/controller/indexController.js
@@ -7,83 +7,20 @@ app.controller('indexCtrl', function ($scope, $http, $document, errorCode, call)
     });
 
     $scope.send = function () {
-
         var to_send = {
             'title':$scope.title,
             'text':$scope.text,
             'source':''
         };
 
-
         call.getCall(to_send).then(function (response) {
             $scope.value = response.data[0];
             $scope.fakeValue = parseInt($scope.value.FAKE * 100);
             $scope.realValue = parseInt($scope.value.REAL * 100);
 
-            var myConfig8 = {
-                "type":"gauge",
-                "scale-r":{
-                    "aperture":200,
-                    "values":"0:100:20",
-                    "center":{
-                        "size":5,
-                        "background-color":"#66CCFF #FFCCFF",
-                        "border-color":"none"
-                    },
-                    "ring":{
-                        "size":10,
-                        "rules":[
-                            {
-                                "rule":"%v >= 0 && %v <= 20",
-                                "background-color":"blue"
-                            },
-                            {
-                                "rule":"%v >= 20 && %v <= 40",
-                                "background-color":"green"
-                            },
-                            {
-                                "rule":"%v >= 40 && %v <= 60",
-                                "background-color":"yellow"
-                            },
-                            {
-                                "rule":"%v >= 60 && %v <= 80",
-                                "background-color":"orange"
-                            },
-                            {
-                                "rule":"%v >= 80 && %v <=100",
-                                "background-color":"red"
-                            }
-                        ]
-                    },
-                    "labels":["0 %","20 %","40 %","60 %","80 %","100 %"],  //Scale Labels
-                    "item":{  //Scale Label Styling
-                        "font-color":"purple",
-                        "font-family":"Open Sans, serif",
-                        "font-size":12,
-                        "font-weight":"bold",   //or "normal"
-                        "font-style":"normal",   //or "italic"
-                        "offset-r":0,
-                        "angle":"auto"
-                    }
-                },
-                gui : {
-                    contextMenu : {
-                        empty : true
-                    }
-                },
-                "plot":{
-                    "csize":"5%",
-                    "size":"100%",
-                    "background-color":"#000000"
-                },
-                "series":[
-                    {"values":[$scope.fakeValue]}
-                ]
-            };
-
             zingchart.render({
-                id : 'myChart',
-                data : myConfig8,
+                id : 'gaugeFakeness',
+                data : getConfig($scope.fakeValue),
                 height : "100%",
                 width: "100%"
             });
