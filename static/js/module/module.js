592af61c3b6fc8f80afb171e30dc01be8d806b74
Alessandro Pisu <a.pisu@dstech.it>
2018-11-26 15:28:53 +0100

    add claims

--- static/js/module/module.js
+++ static/js/module/module.js
@@ -1,6 +1,19 @@
 var app = angular.module('app', []);
-var base = "/fandango/v0.1/fakeness";
+//var base = "/fandango/v0.1/fakeness";
 
 // var app = angular.module('app', []);
-// var base = "http://0.0.0.0:9800/fandango/v0.1/fakeness";
+ var base = "http://192.168.1.58:9800/fandango/v0.1/fakeness";
 
+app.filter('dashless',function() {
+    return function(input) {
+        if (input) {
+            return input.replace('-', ' ');
+        }
+    }
+});
+
+app.filter('capitalize', function() {
+    return function(input) {
+        return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+    }
+});
