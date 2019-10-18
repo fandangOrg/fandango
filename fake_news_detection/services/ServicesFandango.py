6b3cc834d80f4ed4feea8d8874acc7eb8a76ed05
Camila Garcia <c.garcia@ilivetech.it>
2019-10-18 10:25:37 +0200

    new services opendata

--- fake_news_detection/services/ServicesFandango.py
+++ fake_news_detection/services/ServicesFandango.py
@@ -4,7 +4,8 @@ Created on 23 apr 2019
 @author: camila
 '''
 from fake_news_detection.model.InterfacceComunicazioni import News_DataModel, Author_org_DataModel, Media_DataModel, Topics_DataModel,\
- InterfaceInputFeedBack, Claim_input, Claim_output, News, News_annotated
+ InterfaceInputFeedBack, Claim_input, Claim_output, News, News_annotated,\
+    Open_Data
 from ds4biz_commons.utils.requests_utils import URLRequest
 from fake_news_detection.config.AppConfig import  static_folder, url_service_media,\
     url_service_authors, url_similar_claims, template_path
@@ -29,9 +30,8 @@ from flask.globals import request
 log = getLogger(__name__)
 service_scrapy=ScrapyService()
 service_analyzer=AnalyticsService()
-###run deamon
+###run deamon()  uncomment if you want to start kafka deamon
 
-#daemon_run()
 
 headers = {'content-type': "application/json",'accept': "application/json"}
 
@@ -45,43 +45,21 @@ def start_daemon() -> str:
 def analyzer(news_preprocessed:News_DataModel) -> str:
     log.info('''ANALISI NEWS'''+str(news_preprocessed.sourceDomain))
     prest=service_analyzer.analynzer(news_preprocessed)
-#===============================================================================
-# =======
-#     log.info('''ANALISI NEWS''')
-#     model = daopredictor.get_by_id(nomhttp://www.ansa.it/robots.txte_modello)
-#     df = pd.DataFrame(data={'title': [news_preprocessed.headline], 'text': [news_preprocessed.articleBody.replace("\n"," ")]})
-#     
-#     prest = model.predict_proba(df)
-#     prest = pd.DataFrame(prest, columns=model.predictor_fakeness.predictor.predictor.classes_)
-# >>>>>>> develop-0.4
-#===============================================================================    
+    
     print(prest)
     #log.info(json.loads(prest))
     return prest
-    #===========================================================================
-    # log.info(json.loads(prest.to_json(orient='records')))
-    # return json.loads(prest.to_json(orient='records'))
-    #===========================================================================
 
-
-#===============================================================================
-# 
-# def analyzer(daopredictor,nome_modello,news_preprocessed:News_DataModel) ->  str:
-#     
-#     
-#     log.info('''ANALISI NEWS''')
-#     model = daopredictor.get_by_id(nome_modello)
-#     df = pd.DataFrame(data={'title': [news_preprocessed.headline], 'text': [news_preprocessed.articleBody.replace("\n"," ")]})
-#     print(df.columns)
-#     prest = model.predict_proba(df)
-#     prest = pd.DataFrame(prest, columns=model.predictor_fakeness.predictor.predictor.classes_)
-#     log.info(json.loads(prest.to_json(orient='records')))
-#     result = json.loads(prest.to_json(orient='records'))
-#     
-#     return str(result[0]['REAL'])
-#===============================================================================
-
-
+def get_opendata(opendata:Open_Data) -> list:
+    headers = {'content-type': "application/json",'accept': "application/json"}
+    u = URLRequest(url_similar_claims+"/fandango/v0.1/siren/FindFact")
+    log.info("open data sent for:"+ str(opendata.text))
+    payload = {"text": opendata.text ,"category": opendata.category,"topics": opendata.topics}
+    print("OPEND DATA REQUEST:  ",payload)
+    j = json.dumps(payload)
+    req = u.post(data=j, headers= headers)
+    print(req)
+    return req['results']
 
 def get_languages() -> List[Language]:
     l= list()
@@ -92,51 +70,10 @@ def get_languages() -> List[Language]:
 
 def feedback(info:InterfaceInputFeedBack) -> str:
     log.info(info)
-    #===========================================================================
-    # model=daopredictor.get_by_id(nome_modello)
-    # df = pd.DataFrame(data={'title': [info.title], 'text': [info.text.replace("\n", " ")],'label': [info.label.replace("\n", " ")]})
-    # model.partial_fit(df)
-    # daopredictor.update(model)
-    #===========================================================================
     return "OK"
 
 
-#def crawl_online(url:str) -> News_raw: 
-    
-    
-    
-#===============================================================================
-# def preprocessing_online(raw_news:News_raw) -> News_DataModel:
-#     
-#     payload = raw_news.__dict__
-#     payload["fakeness" ]= ""
-#     #===========================================================================
-#     # payload = {"fakeness" : "",
-#     #            "source_domain": raw_news.source_domain,
-#     #            "description": raw_news.description,
-#     #            "videos":raw_news.videos,
-#     #            "title": raw_news.title,
-#     #            "language": raw_news.language,
-#     #            "text": raw_news.text,
-#     #            "date_modified": raw_news.date_modified,
-#     #            "spider": raw_news.spider,
-#     #            "summary": raw_news.summary,
-#     #            "url": raw_news.url,
-#     #            "keywords": raw_news.keywords,
-#     #            "authors": raw_news.authors,
-#     #            "images": raw_news.images,
-#     #            "date_created": raw_news.date_created,
-#     #            "top_image": raw_news.top_image,
-#     #            "texthash": raw_news.texthash
-#     #            }
-#     #===========================================================================
-#      
-#     u = URLRequest(url_service_upm+"/preprocess/article")
-#     j = json.dumps(payload)
-#     response = u.post(data=j, headers=headers)
-#     print(response)
-#     return News_DataModel(**response)
-#===============================================================================
+
 
 class request_craw():
     def __init__(self,url:str,old:str=False):
@@ -153,9 +90,13 @@ def crawl_prep(url:str,old:str="False") -> News_DataModel:
     prest=service_analyzer.analyzer(news_preprocessed,old=old)
     news_preprocessed.results=prest
     news_preprocessed.similarnews = similar_news(news_preprocessed.identifier)
-    print(news_preprocessed.__dict__)
-    return news_preprocessed
-#===============================================================================
+    topics = []
+    #topics = topics_getter(news_preprocessed)
+    opendata = Open_Data(text=news_preprocessed.headline, category=news_preprocessed.headline, topics= topics)
+    op = get_opendata(opendata)
+    prest = {"news_preprocessed": news_preprocessed , "opendata" : op}
+    print(prest)
+    return prest
 
 
 def ping_image(id:str) -> News_DataModel:
@@ -239,16 +180,15 @@ def similar_claims(claim_input: Claim_input) -> list:
     headers = {"Content-Type":  "application/json"}
     j = json.dumps(payload)
     response = u.post(data=j,headers = headers)
-    list_claims = []
+   
     for i in response['results']: 
         print(i)
-        #=======================================================================
-        # for j in i['claimReviews']:
-        #     print("claim",j)
-        #     list_claims.append({"text" : i['text'], "datePublished":i['datePublished'], "reviewBody":j['reviewBody']})
-        #=======================================================================
-    
-    return response['results']
+    opendata = Open_Data(text=claim_input.text, category=claim_input.text, topics= claim_input.topics)
+    op = get_opendata(opendata)
+    result = {"similar_claim":response['results'], "open_data": op }
+    print(result)
+    return result
+
 
 def similar_news(id_news:str) -> list:
     print("start similar news")
@@ -263,6 +203,63 @@ def similar_news(id_news:str) -> list:
             i["calculatedRatingDetail"]["textRating"]=1.0
     return response["results"]
 
+
+
+#------------------------------------>App FLASK <----------------------------------
+
+
+app=DS4BizFlask(__name__,static_folder=static_folder+"/dist/",static_url_path="",template_folder='../templates/')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('index.html')
+
+app.root="/fandango/v0.3/fakeness"
+app.name="FANDANGO"
+#app.add_service("crawl_online", crawl_online, method= 'POST')
+#app.add_service("preprocessing_online", preprocessing_online, method = 'POST')
+app.add_service("crawl_and_preprocessing",crawl_prep, method = 'GET')
+#app.add_service("author_and_organizations",author_org_getter, method = 'POST')
+#app.add_service("Medias", media_getter, method = 'POST')
+#app.add_service("topics and entities",topics_getter , method = 'POST' )
+#app.add_service("aggregator", finalaggr, method= 'POST')
+app.add_service("get_opendata", get_opendata, method= 'POST')
+app.add_service("get_languages",get_languages, method = 'GET')
+app.add_service("analyzer",analyzer, method='POST')
+app.add_service("feedback",feedback, method='POST')
+app.add_service("start_daemon",start_daemon, method='POST')
+app.add_service("info_score",info_score, method = 'GET')
+app.add_service("ping_image",ping_image, method = 'GET')
+app.add_service("ping_video",ping_video, method = 'GET')
+app.add_service("similar_claims",similar_claims, method = 'POST')
+app.add_service("url_image_score",url_image_score, method = 'GET')
+app.add_service("url_video_score",url_video_score, method = 'GET')
+app.add_service("similar_news",similar_news, method = 'POST')
+
+
+CORS(app)
+    
+log.info("RUN ON {cfg}".format(cfg= AppConfig.BASEURL+AppConfig.BASEPORT))
+app.setup()
+app.run(host = "0.0.0.0", port = AppConfig.BASEPORT,debug=False)
+
+#===============================================================================
+# 
+# def analyzer(daopredictor,nome_modello,news_preprocessed:News_DataModel) ->  str:
+#     
+#     
+#     log.info('''ANALISI NEWS''')
+#     model = daopredictor.get_by_id(nome_modello)
+#     df = pd.DataFrame(data={'title': [news_preprocessed.headline], 'text': [news_preprocessed.articleBody.replace("\n"," ")]})
+#     print(df.columns)
+#     prest = model.predict_proba(df)
+#     prest = pd.DataFrame(prest, columns=model.predictor_fakeness.predictor.predictor.classes_)
+#     log.info(json.loads(prest.to_json(orient='records')))
+#     result = json.loads(prest.to_json(orient='records'))
+#     
+#     return str(result[0]['REAL'])
+#===============================================================================
 
 # def finalaggr(news_preprocessed:News_DataModel)-> str:
 #     
@@ -360,55 +357,6 @@ def similar_news(id_news:str) -> list:
 # 
 #     return('ciao')
 #===============================================================================
-
-
-
-
-
-    
-    
-    
-    
-    
-#------------------------------------>DECODING ELASTIC ID SERVICES<----------------------------------
-
-app=DS4BizFlask(__name__,static_folder=static_folder+"/dist/",static_url_path="",template_folder='../templates/')
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('index.html')
-
-
-app.root="/fandango/v0.3/fakeness"
-app.name="FANDANGO"
-#app.add_service("crawl_online", crawl_online, method= 'POST')
-#app.add_service("preprocessing_online", preprocessing_online, method = 'POST')
-app.add_service("crawl_and_preprocessing",crawl_prep, method = 'GET')
-#app.add_service("author_and_organizations",author_org_getter, method = 'POST')
-#app.add_service("Medias", media_getter, method = 'POST')
-#app.add_service("topics and entities",topics_getter , method = 'POST' )
-#app.add_service("aggregator", finalaggr, method= 'POST')
-app.add_service("get_languages",get_languages, method = 'GET')
-app.add_service("analyzer",analyzer, method='POST')
-app.add_service("feedback",feedback, method='POST')
-app.add_service("start_daemon",start_daemon, method='POST')
-app.add_service("info_score",info_score, method = 'GET')
-app.add_service("ping_image",ping_image, method = 'GET')
-app.add_service("ping_video",ping_video, method = 'GET')
-app.add_service("similar_claims",similar_claims, method = 'POST')
-app.add_service("url_image_score",url_image_score, method = 'GET')
-app.add_service("url_video_score",url_video_score, method = 'GET')
-app.add_service("similar_news",similar_news, method = 'POST')
-
-
-CORS(app)
-    
-log.info("RUN ON {cfg}".format(cfg= AppConfig.BASEURL+AppConfig.BASEPORT))
-app.setup()
-app.run(host = "0.0.0.0", port = AppConfig.BASEPORT,debug=False)
-
-
 
 
 
